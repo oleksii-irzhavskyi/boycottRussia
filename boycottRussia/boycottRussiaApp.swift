38413ff4a5738f62fd4f1d0a9d8bd8ad126c125b
Oleksii <oleksii.irzhavskyi@gmail.com>
2022-05-04 17:37:15 +0300

    start

---
+++ boycottRussia/boycottRussiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  boycottRussiaApp.swift
+//  boycottRussia
+//
+//  Created by Oleksii Irzhavskyi on 04.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct boycottRussiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
