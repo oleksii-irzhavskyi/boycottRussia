9c5bc284160c36edbe1fc0d60741b6e0eda6304e
Oleksii <oleksii.irzhavskyi@gmail.com>
2022-07-22 00:58:34 +0300

    add notification

--- boycottRussia/boycottRussiaApp.swift
+++ boycottRussia/boycottRussiaApp.swift
@@ -8,15 +8,34 @@
 import SwiftUI
 import FirebaseCore
 import Firebase
+import Siren
 
 @main
 struct BoycottRussiaApp: App {
     init() {
             FirebaseApp.configure()
+        hyperCriticalRulesExample()
         }
     var body: some Scene {
         WindowGroup {
             CustomTabBar()
+        }
+    }
+    func hyperCriticalRulesExample() {
+        let siren = Siren.shared
+        siren.rulesManager = RulesManager(globalRules: .annoying)
+        siren.presentationManager = PresentationManager(forceLanguageLocalization: .ukrainian)
+
+        siren.wail { results in
+            switch results {
+            case .success(let updateResults):
+                print("AlertAction ", updateResults.alertAction)
+                print("Localization ", updateResults.localization)
+                print("Model ", updateResults.model)
+                print("UpdateType ", updateResults.updateType)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
         }
     }
 }
